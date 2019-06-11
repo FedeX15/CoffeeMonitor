27243bd2880ba9eeab04bf4ceb9ea0d85225eb8b
Federico Matteoni <federicomatteoni@live.it>
2019-06-11 17:26:23 +0200

    Working on widget config Still a mess

---
+++ app/src/main/java/com/fexed/coffeecounter/AddWidgetCnfg.java
@@ -0,0 +1,38 @@
+package com.fexed.coffeecounter;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.arch.persistence.room.Room;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RemoteViews;
+import android.widget.Spinner;
+
+public class AddWidgetCnfg extends Activity {
+
+    @Override
+    public void onCreate(Bundle icile) {
+        super.onCreate(icile);
+
+        View form = LayoutInflater.from(getBaseContext()).inflate(R.layout.addwdgtcnfglayout, null, false);
+        AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(getBaseContext());
+        Spinner coffetypespinner = form.findViewById(R.id.coffeetypespinner);
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "typedb").allowMainThreadQueries().build();
+        coffetypespinner.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line, db.coffetypeDao().getAll().toArray()));
+
+        dialogbuilder.setView(form);
+        dialogbuilder.create();
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+        if (extras != null)
+            appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) finish();
+        RemoteViews views = new RemoteViews(getApplicationContext().getPackageName(), R.layout.addwidgetlayout);
+    }
+}
--- app/src/main/java/com/fexed/coffeecounter/AddWidgetPrvdr.java
+++ app/src/main/java/com/fexed/coffeecounter/AddWidgetPrvdr.java
@@ -1,29 +1,10 @@
 package com.fexed.coffeecounter;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
-import android.widget.RemoteViews;
 
 public class AddWidgetPrvdr extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        final int N = appWidgetIds.length;
-
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int appWidgetId : appWidgetIds) {
-            // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, Dashboard.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.addwidgetlayout);
-            views.setOnClickPendingIntent(R.id.wdgtaddbtn, pendingIntent);
-
-            // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
     }
 }
