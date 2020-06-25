660d41c4e22b74b8b07c3f3af78f40b6d1a0ec06
Federico Matteoni <federicomatteoni@live.it>
2020-06-25 13:05:54 +0200

    Time edit and fewer "new"s

--- app/src/main/java/com/fexed/coffeecounter/ui/adapters/CupRecviewAdapter.java
+++ app/src/main/java/com/fexed/coffeecounter/ui/adapters/CupRecviewAdapter.java
@@ -1,6 +1,7 @@
 package com.fexed.coffeecounter.ui.adapters;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -101,71 +103,83 @@ public class CupRecviewAdapter extends RecyclerView.Adapter<CupRecviewAdapter.Vi
                         View form = LayoutInflater.from(context).inflate(R.layout.editcupdialog, null, false);
                         final TextView nametxt = form.findViewById(R.id.editcuptype);
                         final TextView datetxt = form.findViewById(R.id.editcupdate);
+                        final TextView geotxt = form.findViewById(R.id.editcupgeotag);
                         Button confirmbtn = form.findViewById(R.id.editcupconfirmbtn);
                         Button cancelbtn = form.findViewById(R.id.editcupcancelbtn);
 
+                        View.OnLongClickListener editCupLongClick = new View.OnLongClickListener() {
+                            @Override
+                            public boolean onLongClick(View v) {
+                                switch (v.getId()) {
+                                    case R.id.editcuptype:
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                                        builder.setTitle(R.string.selecttype);
+                                        builder.setAdapter(new ArrayAdapter<>(context, R.layout.type_element, types), new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int pos) {
+                                                thiscup.setTypekey(types.get(pos).getKey());
+                                                nametxt.setText(types.get(pos).getName());
+                                            }
+                                        });
+                                        builder.show();
+                                        return true;
+                                    case R.id.editcupdate:
+                                        final Calendar cld = Calendar.getInstance();
+                                        DatePickerDialog newDateDial = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                                            public void onDateSet(DatePicker view, final int year, final int monthOfYear, final int dayOfMonth) {
+                                                final Calendar newDate = Calendar.getInstance();
+                                                newDate.set(year, monthOfYear, dayOfMonth);
+                                                TimePickerDialog newTimeDial = new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
+                                                    @Override
+                                                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                                        newDate.set(year, monthOfYear, dayOfMonth, hourOfDay, minute);
+                                                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyy HH:mm:ss:SSS", Locale.getDefault());
+                                                        final String date = sdf.format(newDate.getTime());
+                                                        sdf = new SimpleDateFormat("yyy/MM/dd", Locale.getDefault());
+                                                        final String day = sdf.format(newDate.getTime());
+                                                        thiscup.setDate(date);
+                                                        thiscup.setDay(day);
+                                                        datetxt.setText(thiscup.toString());
+                                                    }
+                                                }, cld.get(Calendar.HOUR_OF_DAY), cld.get(Calendar.MINUTE), true);
+                                                newTimeDial.show();
+                                            }
+                                        }, cld.get(Calendar.YEAR), cld.get(Calendar.MONTH), cld.get(Calendar.DAY_OF_MONTH));
+                                        newDateDial.show();
+                                        return true;
+                                    default:
+                                        return false;
+                                }
+                            }
+                        };
+
+
+
                         nametxt.setText(getTypeFromKey(thiscup.getTypekey()).getName());
-                        nametxt.setOnLongClickListener(new View.OnLongClickListener() {
-                            @Override
-                            public boolean onLongClick(View view) {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                                builder.setTitle(R.string.selecttype);
-                                builder.setAdapter(new ArrayAdapter<>(context, R.layout.type_element, types), new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int pos) {
-                                        thiscup.setTypekey(types.get(pos).getKey());
-                                        nametxt.setText(types.get(pos).getName());
-                                    }
-                                });
-                                builder.show();
-                                return true;
-                            }
-                        });
-
+                        nametxt.setOnLongClickListener(editCupLongClick);
                         datetxt.setText(thiscup.toString());
-                        datetxt.setOnLongClickListener(new View.OnLongClickListener() {
-                            @Override
-                            public boolean onLongClick(View view) {
-                                Calendar cld = Calendar.getInstance();
-                                DatePickerDialog StartTime = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
-                                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                        Calendar newDate = Calendar.getInstance();
-                                        newDate.set(year, monthOfYear, dayOfMonth);
-                                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyy HH:mm:ss:SSS", Locale.getDefault());
-                                        final String date = sdf.format(newDate.getTime());
-                                        sdf = new SimpleDateFormat("yyy/MM/dd", Locale.getDefault());
-                                        final String day = sdf.format(newDate.getTime());
-                                        thiscup.setDate(date);
-                                        thiscup.setDay(day);
-                                        datetxt.setText(thiscup.toString());
-
-                                    }
-                                }, cld.get(Calendar.YEAR), cld.get(Calendar.MONTH), cld.get(Calendar.DAY_OF_MONTH));
-                                StartTime.show();
-                                return true;
-                            }
-                        });
+                        datetxt.setOnLongClickListener(editCupLongClick);
 
                         dialogbuilder.setView(form);
                         dialogbuilder.create();
                         final AlertDialog dialog = dialogbuilder.show();
 
-                        confirmbtn.setOnClickListener(new View.OnClickListener() {
+                        View.OnClickListener editCupClick = new View.OnClickListener() {
                             @Override
-                            public void onClick(View view) {
-                                mDataset.set(holder.getAdapterPosition(), thiscup);
-                                db.cupDAO().update(thiscup);
-                                notifyDataSetChanged();
-                                dialog.dismiss();
+                            public void onClick(View v) {
+                                switch (v.getId()) {
+                                    case R.id.editcupconfirmbtn:
+                                        mDataset.set(holder.getAdapterPosition(), thiscup);
+                                        db.cupDAO().update(thiscup);
+                                        notifyDataSetChanged();
+                                        dialog.dismiss();
+                                    case R.id.editcupcancelbtn:
+                                        dialog.dismiss();
+                                }
                             }
-                        });
-
-                        cancelbtn.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                dialog.dismiss();
-                            }
-                        });
+                        };
+                        confirmbtn.setOnClickListener(editCupClick);
+                        cancelbtn.setOnClickListener(editCupClick);
 
                         return false;
                     default:
