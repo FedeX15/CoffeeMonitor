92057d2f8cf1303e945da90d412573647fa8de4e
Federico Matteoni <federicomatteoni@live.it>
2020-06-26 09:37:22 +0200

    Working on widget

--- app/src/main/java/com/fexed/coffeecounter/sys/widget/AddWidgetCnfg.java
+++ app/src/main/java/com/fexed/coffeecounter/sys/widget/AddWidgetCnfg.java
@@ -4,42 +4,65 @@ import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.RemoteViews;
 import android.widget.Spinner;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.room.Room;
 
 import com.fexed.coffeecounter.R;
+import com.fexed.coffeecounter.data.Coffeetype;
 import com.fexed.coffeecounter.db.AppDatabase;
 
 /**
  * Created by Federico Matteoni on 11/06/2019
  */
 public class AddWidgetCnfg extends Activity {
+    Coffeetype coffeetype = null;
 
     @Override
     public void onCreate(Bundle icile) {
         super.onCreate(icile);
+        setContentView(R.layout.widget_addconfig);
 
-        View form = LayoutInflater.from(getBaseContext()).inflate(R.layout.widget_addconfig, null, false);
-        AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(getBaseContext());
-        Spinner coffetypespinner = form.findViewById(R.id.coffeetypespinner);
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "typedb").allowMainThreadQueries().build();
-        coffetypespinner.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line, db.coffetypeDao().getAll().toArray()));
+        Spinner coffetypespinner = findViewById(R.id.coffeetypespinner);
+        final AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "typedb").allowMainThreadQueries().build();
+        coffetypespinner.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, db.coffetypeDao().getAll().toArray()));
 
-        dialogbuilder.setView(form);
-        dialogbuilder.create();
+        coffetypespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                coffeetype = db.coffetypeDao().getAll().get(position);
+            }
 
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
-        int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
-        if (extras != null)
-            appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-        if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) finish();
-        RemoteViews views = new RemoteViews(getApplicationContext().getPackageName(), R.layout.widget_addlayout);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        Button confirm = findViewById(R.id.editcupconfirmbtn);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                Bundle extras = intent.getExtras();
+                int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+                if (extras != null)
+                    appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+                if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) finish();
+                RemoteViews views = new RemoteViews(getApplicationContext().getPackageName(), R.layout.widget_addlayout);
+                views.setString(R.id.wdgttxtv, "setText", coffeetype.getName());
+                finish();
+            }
+        });
+        Button cancel = findViewById(R.id.editcupcancelbtn);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
