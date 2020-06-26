92f412ba8c0fce9eb6032d079e8f3802936bada9
Federico Matteoni <federicomatteoni@live.it>
2020-06-26 10:33:29 +0200

    Clean start

--- app/src/main/java/com/fexed/coffeecounter/sys/widget/AddWidgetCnfg.java
+++ app/src/main/java/com/fexed/coffeecounter/sys/widget/AddWidgetCnfg.java
@@ -1,23 +1,10 @@
 package com.fexed.coffeecounter.sys.widget;
 
 import android.app.Activity;
-import android.app.PendingIntent;
-import android.appwidget.AppWidgetManager;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.RemoteViews;
-import android.widget.Spinner;
-
-import androidx.room.Room;
 
 import com.fexed.coffeecounter.R;
 import com.fexed.coffeecounter.data.Coffeetype;
-import com.fexed.coffeecounter.db.AppDatabase;
-import com.fexed.coffeecounter.ui.MainActivity;
 
 /**
  * Created by Federico Matteoni on 11/06/2019
@@ -29,50 +16,5 @@ public class AddWidgetCnfg extends Activity {
     public void onCreate(Bundle icile) {
         super.onCreate(icile);
         setContentView(R.layout.widget_addconfig);
-
-        Spinner coffetypespinner = findViewById(R.id.coffeetypespinner);
-        final AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "typedb").allowMainThreadQueries().build();
-        coffetypespinner.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, db.coffetypeDao().getAll().toArray()));
-
-        coffetypespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                coffeetype = db.coffetypeDao().getAll().get(position);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
-
-        Button confirm = findViewById(R.id.editcupconfirmbtn);
-        confirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = getIntent();
-                Bundle extras = intent.getExtras();
-                int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
-                if (extras != null)
-                    appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-                if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) finish();
-                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getApplicationContext());
-                RemoteViews views = new RemoteViews(getApplicationContext().getPackageName(), R.layout.widget_addlayout);
-                views.setString(R.id.wdgttxtv, "setText", coffeetype.getName());
-                intent = new Intent(getApplicationContext(), MainActivity.class);
-                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
-                views.setOnClickPendingIntent(R.id.wdgtaddbtn, pendingIntent);
-                appWidgetManager.updateAppWidget(appWidgetId, views);
-                Intent resultValue = new Intent();
-                resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-                setResult(RESULT_OK, resultValue);
-                finish();
-            }
-        });
-        Button cancel = findViewById(R.id.editcupcancelbtn);
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
     }
 }
