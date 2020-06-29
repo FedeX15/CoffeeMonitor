2c7d21af9038fbc6708b9f9eae21ad7242115044
Federico Matteoni <federicomatteoni@live.it>
2020-06-29 14:29:57 +0200

    GServices check

--- app/src/main/java/com/fexed/coffeecounter/ui/MapActivity.java
+++ app/src/main/java/com/fexed/coffeecounter/ui/MapActivity.java
@@ -1,12 +1,15 @@
 package com.fexed.coffeecounter.ui;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.fragment.app.FragmentActivity;
 
 import com.fexed.coffeecounter.R;
 import com.fexed.coffeecounter.data.Coffeetype;
 import com.fexed.coffeecounter.data.Cup;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -26,8 +29,21 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        switch (api.isGooglePlayServicesAvailable(this)) {
+            case ConnectionResult.SUCCESS:
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                mapFragment.getMapAsync(this);
+                break;
+            case ConnectionResult.SERVICE_MISSING:
+                Toast.makeText(this, "Google Play Services Missing", Toast.LENGTH_SHORT).show();
+                break;
+            case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+                Toast.makeText(this, "Update Google Play Services", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this, api.isGooglePlayServicesAvailable(this), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
