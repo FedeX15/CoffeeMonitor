066c43689d6a57b3e9e207b55b79c60f6139b0fb
Federico Matteoni <federicomatteoni@live.it>
2020-06-30 09:51:04 +0200

    Commenting

--- app/src/main/java/com/fexed/coffeecounter/ui/CaptureActivityPortrait.java
+++ app/src/main/java/com/fexed/coffeecounter/ui/CaptureActivityPortrait.java
@@ -4,7 +4,6 @@ import com.journeyapps.barcodescanner.CaptureActivity;
 
 /**
  * Activity used for the QR code scanner in portrait mode
- * <p>
  * Created by Federico Matteoni on 18/06/2020
  */
 public class CaptureActivityPortrait extends CaptureActivity {}
